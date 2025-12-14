baab18be023f7aff51d664a3b067b0bd14f4fa94
Farshid-Fardnian <ferisevenstar@gmail.com>
2025-12-14 20:37:50 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 document.getElementById("btn").addEventListener("click", () => {
   alert("دکمه کلیک شد!");
 });
+
+// تست GET
+fetch("/api/users")
+  .then(res => res.json())
+  .then(data => {
+    console.log("Users:", data);
+  });
+
+// تست POST
+fetch("/api/users", {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({ name: "فرشید" })
+});
+
+
