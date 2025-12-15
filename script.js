439f0c275c1346c904f234a337fd7fe6f04b3795
Farshid-Fardnian <ferisevenstar@gmail.com>
2025-12-15 12:55:48 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+  // ارسال OTP
+  document.getElementById("sendOtp")?.addEventListener("click", async () => {
+    const mobile = document.getElementById("mobile").value;
 
-
-  console.log("برو 2");
-
-
-
- 
-});
-
-
-document.getElementById("sendOtp").addEventListener("click", () => {
-  alert("دکمه کلیک شد!");
-       const mobile = document.getElementById("mobile").value;
-
-    const res = fetch("/send-otp", {
+    const res = await fetch("/send-otp", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ mobile })
     });
 
-    const data = res.json();
+    const data = await res.json();
     alert(data.message);
 
     if (data.success) {
       document.getElementById("mobile-box").style.display = "none";
       document.getElementById("otp-box").style.display = "block";
     }
-});
+  });
 
-
-
-document.getElementById("verifyOtp").addEventListener("click", () => {
-       const mobile = document.getElementById("mobile").value;
+  // تایید OTP
+  document.getElementById("verifyOtp")?.addEventListener("click", async () => {
+    const mobile = document.getElementById("mobile").value;
     const otp = document.getElementById("otp").value;
 
-    const res = fetch("/verify-otp", {
+    const res = await fetch("/verify-otp", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ mobile, otp })
     });
 
-    const data = res.json();
+    const data = await res.json();
     alert(data.message);
 
     if (data.success) {
       alert("🎉 ورود موفق");
     }
+  });
+
 });
-
- 
-
-
-
