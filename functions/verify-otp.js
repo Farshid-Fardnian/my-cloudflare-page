efb12912c1ad3fbf7072735d25411e3b7f1b2086
Farshid-Fardnian <ferisevenstar@gmail.com>
2025-12-15 17:19:13 +0330

    Update verify-otp.js

--- functions/verify-otp.js
+++ functions/verify-otp.js
@@ -1,4 +1,16 @@
+
+import { corsHeaders } from "./cors";
+
 export async function onRequestPost({ request, env }) {
+
+      const headers = corsHeaders(request);
+
+  // ✅ پاسخ به Preflight
+  if (request.method === "OPTIONS") {
+    return new Response(null, { headers });
+  }
+
+  
   try {
     const { mobile, otp } = await request.json();
 
