61ee1c33c0320b60e893e0c7504c28e1aea6fd7c
Farshid-Fardnian <ferisevenstar@gmail.com>
2025-12-15 10:18:54 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,62 +1,47 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("script.js loaded");
 
-  
+  const sendBtn = document.getElementById("sendBtn");
+  const verifyBtn = document.getElementById("verifyBtn");
+
+  console.log("همه چیز OK");
+
+  sendBtn.addEventListener("click", sendOtp);
+  verifyBtn.addEventListener("click", verifyOtp);
+
+  async function sendOtp() {
+    const mobile = document.getElementById("mobile").value;
+
+    const res = await fetch("/send-otp", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ mobile })
+    });
+
+    const data = await res.json();
+    alert(data.message);
+
+    if (data.success) {
+      document.getElementById("mobile-box").style.display = "none";
+      document.getElementById("otp-box").style.display = "block";
+    }
+  }
+
+  async function verifyOtp() {
+    const mobile = document.getElementById("mobile").value;
+    const otp = document.getElementById("otp").value;
+
+    const res = await fetch("/verify-otp", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ mobile, otp })
+    });
+
+    const data = await res.json();
+    alert(data.message);
+
+    if (data.success) {
+      alert("🎉 ورود موفق");
+    }
+  }
+
 });
-
-
-
-
-// document.getElementById("btn").addEventListener("click", () => {
-//  fetch("/api/users", {
-//   method: "POST",
-//   headers: { "Content-Type": "application/json" },
-//   body: JSON.stringify({ name: "صادق" })
-// });
-
-// });
-
-
-function sendOtp() {
-  const mobile = document.getElementById("mobile").value;
-
-  const res = await fetch("/send-otp", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ mobile })
-  });
-
-  const data = await res.json();
-  alert(data.message);
-
-  if (data.success) {
-    document.getElementById("mobile-box").style.display = "none";
-    document.getElementById("otp-box").style.display = "block";
-  }
-}
-
-function verifyOtp() {
-  const mobile = document.getElementById("mobile").value;
-  const otp = document.getElementById("otp").value;
-
-  const res = await fetch("/verify-otp", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ mobile, otp })
-  });
-
-  const data = await res.json();
-  alert(data.message);
-
-  if (data.success) {
-    alert("🎉 ورود موفق!");
-    // redirect or set cookie
-  }
-}
-
-
-
-
-
-
-
